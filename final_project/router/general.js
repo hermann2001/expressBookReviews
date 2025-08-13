0c9b57bbfa8aa30851b46bb9ba57685e6ce1740e
hermann2001 <adreanodalmeida@gmail.com>
2025-08-13 08:57:07 -0400

    Tache 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,7 +27,11 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+
+  const bookDetails = Object.entries(books).filter(([isbn, b]) => b.author === author);
+
+  return res.status(200).json(JSON.stringify(bookDetails));
 });
 
 // Get all books based on title
