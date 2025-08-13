4bafc47789b2405a0cdc68177638cadb02200aca
hermann2001 <adreanodalmeida@gmail.com>
2025-08-13 08:00:36 -0400

    Middleware authentication

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,22 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
+app.use("/customer/auth/*", function auth(req, res, next){
 //Write the authenication mechanism here
+    if(req.session.authorization) {
+        let token = req.session.authorization["accessToken"];
+
+        jwt.verify(token, 'accessUser', (err, user) => {
+            if (err) {
+                return res.status(403).json({message : "User not authenticated !"})
+            } else {
+                req.user = user;
+                next()
+            }
+        })
+    } else {
+        return res.status(403).json({message : "User not looged in !"})
+    }
 });
  
 const PORT =5000;
