5a6bea00b0c8e165c880a9c9d9c3b5812a5f5f44
hermann2001 <adreanodalmeida@gmail.com>
2025-08-13 09:52:05 -0400

    Tache 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -21,12 +21,37 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) return res.status(404).json({message: "Unable username and password !"});
+
+  if (!authenticatedUser(username, password)) return res.status(404).json({message: "Invalid Login. Check username and password !"});
+
+  let accessToken = jwt.sign({
+    data : password
+  }, "accessUser", { expiresIn : 2 * 60 * 60});
+
+  req.session.authorization = {
+    accessToken, username
+  };
+
+  return res.status(200).json({message: "You are logged in !"});
 });
 
 // Add a book review
